1f018b7b3e0a2b06763e8c8086b6a0fe27b345c1
MinayeAliyeva <7s346pu@code.edu.az>
2023-12-04 23:12:01 +0400

    added changes

--- booking/src/layout/header/Header.tsx
+++ booking/src/layout/header/Header.tsx
@@ -1,14 +1,14 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header>
-  <Link to='/'>Home</Link>
-  <Link to='/about'>About</Link>
-  <Link to='/signUp'>Sign Up</Link>
-  <Link to='/login'>Login</Link>
+    <header className="flex items-center place-content-around p-8 bg-slate-300">
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/signUp">Sign Up</Link>
+      <Link to="/login">Login</Link>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
