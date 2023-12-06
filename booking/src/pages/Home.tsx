644ef203c17b26a225d816722549bd648a23b0b7
MinayeAliyeva <7s346pu@code.edu.az>
2023-12-06 23:08:51 +0400

    added changes

--- booking/src/pages/Home.tsx
+++ booking/src/pages/Home.tsx
@@ -1,24 +1,31 @@
-const PRODUCTS_URL = "https://jsonplaceholder.typicode.com/users";
 import axios from "axios";
 import { useEffect, useState } from "react";
-const Home = () => {
-  const [data, setData] = useState([]);
-  const fetchdata = async () => {
-    const response = await axios.get(PRODUCTS_URL);
-    const data = await response.data;
+
+interface IProduct {
+  id: number;
+  name: string;
+  username: string;
+}
+const Home = (): JSX.Element => {
+  const PRODUCTS_URL = "https://jsonplaceholder.typicode.com/users";
+  const [data, setData] = useState<IProduct[]>([]);
+  const fetchdata = async (): Promise<IProduct[]> => {
+    const response = await axios.get<IProduct[]>(PRODUCTS_URL);
+    const data = response.data;
     setData(data);
     return data;
   };
-  console.log(data);
 
   useEffect(() => {
     fetchdata();
   }, []);
-const deleteProduct=async(id)=>{
-await axios.delete(`${PRODUCTS_URL}/${id}`)
-const filteredData=data.filter((obj)=> obj.id!=id);
-setData(filteredData)
-}
+  const deleteProduct = async (id: number): Promise<void> => {
+    await axios.delete(`${PRODUCTS_URL}/${id}`);
+    const filteredData = data.filter((obj) => obj.id !== id);
+    setData(filteredData);
+  };
+  console.log(data);
+
   return (
     <section id="products">
       <h2>Product List:</h2>
@@ -30,7 +37,12 @@ setData(filteredData)
               {obj.username}
               <div>
                 {" "}
-                <button onClick={()=>deleteProduct(obj.id)} className="deleteBtn">Delete</button>
+                <button
+                  onClick={() => deleteProduct(obj.id)}
+                  className="deleteBtn"
+                >
+                  Delete
+                </button>
               </div>
             </div>
           </div>
--- booking/src/pages/SignUp.tsx
+++ booking/src/pages/SignUp.tsx
@@ -3,31 +3,35 @@ import axios from "axios";
 const USERS_URL = "http://localhost:3000/users";
 
 const SignUp = () => {
-  const initialData = {
+  interface IUser{
+    name:string,
+    sname:string
+  }
+  const initialData :IUser= {
     name: "",
     sname: "",
   };
-  const [user, setUser] = useState(initialData);
-  const handleChange = (e) => {
+  const [user, setUser] = useState<IUser>(initialData);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setUser((prevs) => ({
       ...prevs,
       [name]: value,
     }));
   };
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     axios.post(USERS_URL, user);
     setUser(initialData)
   };
   return (
-    <form>
+    <form onSubmit={(e) => handleSubmit(e)} >
       <h1>Sign Up</h1>
       <label>Name</label>
       <input name="name" onChange={(e) => handleChange(e)} />
       <label>Surname</label>
       <input name="sname" onChange={(e) => handleChange(e)} />
-      <button onClick={(e) => handleSubmit(e)}>SignUp</button>
+      <button type="submit" >SignUp</button>
     </form>
   );
 };
